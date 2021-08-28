c4f1f361017249d30320fbc52505da482e2cfe95
Gustavo Priebe <gustavo_priebe@hotmail.com>
2021-08-28 16:40:01 -0300

    final code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,8 @@
 // DOM
 let clock = document.querySelector(".clock");
-const [start, pause, restart] = [
-    document.querySelector(".button__begin"),
-    document.querySelector(".button__pause"),
-    document.querySelector(".button__restart"),
-];
 
 // Adicionar 0 a esqueda de números < 10
-const zeroAEsquerda = (x) => x >= 10 ? x : `0${x}`;
+const zeroAEsquerda = (x) => (x >= 10 ? x : `0${x}`);
 
 // Variáveis de tempo
 let [hour, minute, second] = [0, 0, 0];
@@ -24,23 +19,39 @@ function timer() {
         minute = 0;
         hour++;
     }
-    clock.innerHTML = `<p>${zeroAEsquerda(hour)}:${zeroAEsquerda(minute)}:${zeroAEsquerda(second)}</p>`
+    clock.innerHTML = `<p>${zeroAEsquerda(hour)}:${zeroAEsquerda(
+        minute
+    )}:${zeroAEsquerda(second)}</p>`;
 }
 
 // Funções do timer
-const pressStart= () => {
+const pressStart = () => {
+    clearInterval(cron);
     cron = setInterval(timer, 1000);
+    clock.style.color = "black";
 };
-const pressPause= () => {
-    clearInterval(cron), 1000
+const pressPause = () => {
+    clearInterval(cron);
+    clock.style.color = "red";
 };
-const pressRestart= () => {
-    hour,minute,second = 0, 0, 0;
-    clock.innerText = '00:00:00';
+const pressRestart = () => {
+    hour, minute, (second = 0), 0, 0;
+    clock.innerText = "00:00:00";
     pressPause();
-}
+    clock.style.color = "black";
+};
 
 // Interação dos botões
-start.addEventListener('click', pressStart)
-pause.addEventListener('click', pressPause)
-restart.addEventListener('click', pressRestart)
+document.addEventListener("click", function (e) {
+    const element = e.target;
+
+    if (element.classList.contains("button__begin")) {
+        pressStart();
+    }
+    if (element.classList.contains("button__pause")) {
+        pressPause();
+    }
+    if (element.classList.contains("button__restart")) {
+        pressRestart();
+    }
+});
